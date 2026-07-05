a26d01d0366fb744411ca6192e4160090805315b
Lfierro2006 <lfierro2025@inf.udec.cl>
2026-07-05 04:08:53 -0400

    Agregar respectivo javadoc

--- src/Main/Java/logicatienda/animales/estados/Triste.java
+++ src/Main/Java/logicatienda/animales/estados/Triste.java
@@ -2,7 +2,19 @@ package logicatienda.animales.estados;
 
 import logicatienda.animales.Animal;
 
+/**
+ * Estado que representa a un animal triste.
+ * La felicidad disminuye rápidamente con el tiempo.
+ * Permite jugar, curar y limpiar.
+ */
+
 public class Triste implements EstadoAnimal {
+
+    /**
+     * Ejecuta el comportamiento del estado triste.
+     * Disminuye la felicidad en 4 puntos por ciclo.
+     * @param animal El animal al que se aplica el estado
+     */
     @Override
     public void ejecutar(Animal animal) {
         animal.disminuirNivel(Animal.Estadistica.FELICIDAD, 4);
