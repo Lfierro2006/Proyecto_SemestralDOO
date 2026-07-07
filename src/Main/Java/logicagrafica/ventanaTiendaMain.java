667691c306a06c2d017b2d5fcc205ac5984b9d01
kalotres <jsa.molina.diaz@gmail.com>
2026-07-06 21:22:02 -0400

    Añadido muestra grafica del presupuesto personal y del fondo de la tienda

--- src/Main/Java/logicagrafica/ventanaTiendaMain.java
+++ src/Main/Java/logicagrafica/ventanaTiendaMain.java
@@ -9,23 +9,40 @@ import java.awt.*;
 public class ventanaTiendaMain extends JFrame {
     public final int width = 1280;
     public final int height = 800;//PROPORCION 8:5
-    public ventanaTiendaMain(){
-        //DEBO AÑADIR UN JLABEL PARA EL PRESUPUESTO
+    private Tienda tiendalogica;
+    private Mostrador mostrador;
+    private JLabel Presupuesto;
+    private Runnable actualizarP;
+    public ventanaTiendaMain(){ //NOTA: ESTA VENTANA NO TIENE AJUSTE DINAMICO DE NINGUN TIPO, SI SE QUIERE CAMBIAR SU RESOLUCION SE DEBEN CAMBIAR LOS VALORES width y height y volver a ejecutar la virtual machine
+
         this.setTitle("Tienda Principal");
         this.setSize(width, height); // En caso de querer cambiar la ventana por favor mantener la proporcion de 8:5
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.setLocationRelativeTo(null);
         this.setLayout(null);
-        int alto = height/5-6;
-        int ancho = width/8;
-        Tienda tiendalogica = new Tienda(30000);
+
+        int alto = height/5-7;
+        int ancho = width/8-2;
+
+        tiendalogica = new Tienda(30000);
+        mostrador = new Mostrador(ancho*2,0 ,ancho*6 ,alto*3 ,tiendalogica);
+        this.add (mostrador);
+        Presupuesto = new JLabel("$" + tiendalogica.getPresupuesto());
+
+        Presupuesto.setFont(new Font("Arial", Font.BOLD, 70));
+        Presupuesto.setForeground(new Color(49, 243, 49));
+        Presupuesto.setBounds((int)(ancho*6.3),(int) (alto*2.3),(int)(ancho*1.6),(int)(alto*0.8));
+        this.add(Presupuesto, 0);
+        Runnable ActualizarPresupuesto=()  -> {
+            Presupuesto.setText("$" + tiendalogica.getPresupuesto());
+        };
         CasillaMascota[] casillas = new CasillaMascota[22];
         for (int fila = 0,index=0; fila < 5; fila++) {
             int columnas = (fila <= 2) ? 2 : 8;
             for (int col = 0; col < columnas; col++) {
                 int x = col * ancho;
                 int y = fila * alto;
-                casillas[index] = new CasillaMascota(x, y, ancho, alto, null, tiendalogica);
+                casillas[index] = new CasillaMascota(x, y, ancho, alto, null, tiendalogica, ActualizarPresupuesto);
                 this.add(casillas[index]);
                 index++;
             }
